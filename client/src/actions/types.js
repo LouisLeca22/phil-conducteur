ba1dac6f4aa1bef07ddce7243d9ef4693e794562
leprelou <louisleca@zohomail.eu>
2021-08-06 19:18:41 +0200

    like and unlike

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -15,4 +15,5 @@ export const UPDATE_PROFILE = "UPDATE_PROFILE";
 export const ACCOUNT_DELETED = "ACCOUNT_DELETED";
 export const RESET_PROFILE_LOADING = "RESET_PROFILE_LOADING";
 export const GET_POSTS = "GET_POSTS";
-export const POST_ERROR = "POST_ERROR"
+export const POST_ERROR = "POST_ERROR";
+export const UPDATE_LIKES = "UPDATE_LIKES"
--- client/src/components/posts/PostItem.jsx
+++ client/src/components/posts/PostItem.jsx
@@ -2,8 +2,9 @@ import {Link} from 'react-router-dom'
 import PropTypes from 'prop-types'
 import Moment from "react-moment"
 import {connect} from "react-redux"
+import { addLike, removeLike } from '../../actions/post'
 
-const PostItem = ({auth, post: {_id, text, name, avatar, user, likes, comments, date}}) => (
+const PostItem = ({addLike, removeLike, auth, post: {_id, text, name, avatar, user, likes, comments, date}}) => (
     <div className="post bg-white p-1 my-1">
     <div>
       <Link to={`/post/${_id}`}>
@@ -22,13 +23,13 @@ const PostItem = ({auth, post: {_id, text, name, avatar, user, likes, comments, 
        <p className="post-date">
           Publié le <Moment format='DD/MM/YYYY'>{date}</Moment>
       </p>
-      <button type="button" className="btn btn-light">
+      <button onClick={e => addLike(_id)} type="button" className="btn btn-light">
         <i className="fas fa-thumbs-up"></i> {" "}
         <span>{likes.length > 0  && (
           <span>{likes.length}</span>
         )} </span>
       </button>
-      <button type="button" className="btn btn-light">
+      <button onClick={e => removeLike(_id)} type="button" className="btn btn-light">
         <i className="fas fa-thumbs-down"></i>
       </button>
       <Link to={`/post/${_id}`} className="btn btn-primary">
@@ -58,4 +59,4 @@ const mapStateToProps = state => ({
   auth: state.auth
 })
 
-export default connect(mapStateToProps, {})(PostItem)
+export default connect(mapStateToProps, {addLike, removeLike})(PostItem)
